07cf4dc412b7d9ca2ec6de27697f46f9c17249ac
2401777202 <131423444+2401777202@users.noreply.github.com>
2023-04-21 17:59:51 +0800

    Update Catalog.java

--- GourmetCoffeeSystem/src/base/Catalog.java
+++ GourmetCoffeeSystem/src/base/Catalog.java
@@ -1,4 +1,23 @@
 package base;
 
 public class Catalog {
+  this.product=new ArrayList<Product>();
 }
+public void addProduct(Product product){
+     return this.product.add(product);
+}
+public Iterator<Product>iterator(){
+     return this.products.iterator();
+}
+
+public Product getProduct(String code){
+  for(Ptoduct product:this.products){
+      if(product.getCode().equals(code)){
+        return product;
+      }
+    )
+      return null;
+  }
+  pulic int getNumberOfProducts(){
+    return this.products.size();
+  }
